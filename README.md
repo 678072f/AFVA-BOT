63b04a2bb0120b3d3f08f3ad3d5fc7099cbc7cd0
Daniel Duhon <danielgduhon@gmail.com>
2023-02-07 14:07:14 -0600

    Updated README with known issues and added comments to main.py

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # README
 
 This is a Discord Bot for Air France/KLM Virtual Airlines. The current bot is only an example from a tutorial.
+
+# Known-Issues
+
+1. Need to add code for removing the 'New Pilot' role once verification is complete.
+2. Need to add error handling if the user hasn't registered their account with Discord.
--- main.py
+++ main.py
@@ -34,42 +34,58 @@ async def on_message(message):
 
     print(f"Message {user_message} by {username} on {channel}.")
 
+    # Make sure that the bot only responds to users
     if message.author == client.user:
         return
 
-    if user_message.lower() == "verify":
+    # Check if the user sent "!verify"
+    if user_message.lower() == "!verify":
+        # Respond to the user with a welcome message and send the verification link:
         await message.channel.send(f"Hello {username}, I am sending you to the verification link.")
         await message.channel.send(f'Please open: {registrationURL + user_id} to register your account, then react with 👍.')
 
+        # Define function to check for thumbsup reaction
         def check(reaction, user):
             return user == message.author and str(reaction.emoji) == '👍'
 
         try:
+            # Check for the user's reaction
             reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+            # Store the user's nickname and roles from the verification Function
             nickName, roleList = BC.verifyUser(user_id)
 
+            # Iterate through the roles provided from the server
             for afvaRole in roleList:
+                # Check if the role exists and store in 'role'
                 role = discord.utils.get(member.guild.roles, id = afvaRole)
 
+                # Check if the user already has the role. If yes, skip
                 if afvaRole in member.roles:
                     await message.channel.send(f"You already have the {afvaRole} role! Moving on...")
+                # If the user doesn't already have the role, add it
                 else:
+                    # Make sure the role exists
                     if role is not None:
                         await member.add_roles(role)
                         await message.channel.send("Added role.")
+                    
+                    # Send a message if the role isn't available
                     else:
                         await message.channel.send(f"The role with ID: {afvaRole} was not found on this server!")
-
+                
+                # Print the role (for debugging)
                 print(role)
 
+            # Respond with user's new nickname
             await message.channel.send(f"Success! Hello {nickName}")
 
+        # Catch timeout
         except asyncio.TimeoutError:
             await channel.send('👎')
 
+        # Update Nickname
+        await member.edit(nick=nickName)
         
-        return
-
 @client.event
 async def on_member_join(member):
     username = str(member.id)
